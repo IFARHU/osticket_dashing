5f744a81a0d5dc09b684a283ab25b1918f0efe35
Demostenes Garcia <d.garcia@ifarhu.gob.pa>
2017-01-27 09:36:32 -0500

    Changes based on latest osTicket version (1.10-rc3)

--- jobs/tickets_telecomunicaciones.rb
+++ jobs/tickets_telecomunicaciones.rb
@@ -1,13 +1,13 @@
 by_department = "SELECT
     COUNT(ost_ticket.ticket_id) AS num,
     ost_ticket.status_id,
-    ost_department.dept_name AS dept_name,
+    ost_department.name AS dept_name,
     ost_ticket_status.name AS name
   FROM ost_ticket
   INNER JOIN ost_ticket_status
     ON ost_ticket_status.id = ost_ticket.status_id
   INNER JOIN ost_department
-    ON ost_department.dept_id = ost_ticket.dept_id
+    ON ost_department.id = ost_ticket.dept_id
   WHERE ost_ticket.dept_id = 7
   GROUP BY ost_ticket.status_id, ost_ticket.dept_id
   ORDER BY ost_ticket.dept_id;";
